edc76685e0e3bcd7d7987961b35724c685c56008
flavius-t <flavius.t@hotmail.com>
2022-05-04 20:30:46 +0100

    add led-to-bcm-pin mapping

--- edgepi-leds.js
+++ edgepi-leds.js
@@ -1,14 +1,27 @@
 const { Gpio } = require( 'onoff' );
 
+// dict holding LED # : BCM GPIO PIN # pairs
+// Example: '1':'13' maps LEDY C1 to GPIO13 pin
+const ledToPin = {
+    '1':'13', '2':'12'
+};
+
 module.exports = function(RED) {
     function ledTriggerNode(ledNum) {
         RED.nodes.createNode(this, ledNum);
         var node = this;
 
         node.on('input', function(msg) {
-            // build Gpio object at BCMP pin corresponding to LED light number
-            const outpin = new Gpio(Number(msg.payload), 'out');
-            outpin.writeSync(1)
+            // build Gpio object at BCM pin corresponding to LED light number
+            const ledNum = msg.payload;
+            // toggle LED if LED number is valid
+            if (ledNum in ledToPin) {
+                const outpin = new Gpio(Number(ledToPin[msg.payload]), 'out');
+                outpin.writeSync(1)
+            }
+            else {
+                node.error("LED number does not exist");
+            }
             node.send(msg);
         });
     }
