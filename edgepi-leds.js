29aae0f2d4769dfd31ad9816478ac661947d9aab
flavius-t <flavius.t@hotmail.com>
2022-05-05 17:57:31 +0100

    add user LED number input

--- edgepi-leds.js
+++ edgepi-leds.js
@@ -7,20 +7,21 @@ const ledToPin = {
 };
 
 module.exports = function(RED) {
-    function ledTriggerNode(ledNum) {
-        RED.nodes.createNode(this, ledNum);
+    function ledTriggerNode(config) {
+        RED.nodes.createNode(this, config);
+        this.led = config.led;
         var node = this;
 
         node.on('input', function(msg) {
             // build Gpio object at BCM pin corresponding to LED light number
-            const ledNum = msg.payload;
+            ledNum = node.led
             // toggle LED if LED number is valid
             if (ledNum in ledToPin) {
-                const outpin = new Gpio(Number(ledToPin[msg.payload]), 'out');
-                outpin.writeSync(1)
+                const outpin = new Gpio(Number(ledToPin[ledNum]), 'out');
+                outpin.writeSync(parseInt(msg.payload))
             }
             else {
-                node.error("LED number does not exist");
+                node.error(`LED Number ${ledNum} does not exist.`);
             }
             node.send(msg);
         });
