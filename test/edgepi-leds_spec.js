ef63f9163edf77adcd47e8c6f68055cdfd04eea3
flavius-t <flavius.t@hotmail.com>
2022-05-05 19:26:45 +0100

    expand correct instantiation unit test

--- test/edgepi-leds_spec.js
+++ test/edgepi-leds_spec.js
@@ -14,12 +14,15 @@ describe('led-trigger-node', function () {
     });
 
     // assert node is correctly instantiated
-    it('should be loaded', function (done) {
-        const flow = [{ id: "node_1", type: "led-trigger-node", name: "led-trigger-node" }];
+    it('should be instantiated correctly', function (done) {
+        const flow = [{ id: "node_1", type: "led-trigger-node", name: "led-trigger-node"}];
         helper.load(ledTriggerNode, flow, function () {
           const n1 = helper.getNode("node_1");
           try {
             n1.should.have.property('name', 'led-trigger-node');
+            n1.should.have.property('type', 'led-trigger-node');
+            n1.should.have.property('id', 'node_1');
+            n1.should.have.property('led', undefined);
             done();
           } catch(err) {
             done(err);
